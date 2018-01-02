480abf3a5a94271298db6d12f7e7ca5645b1ae7d
Marcell Endrey <marcell.endrey@prezi.com>
2018-01-02 12:24:18 +0100

    let it be so

---
+++ spoon.rb
@@ -0,0 +1,12 @@
+class Spoon < Formula
+  desc "SSH into EC2 nodes easily"
+  homepage "https://github.com/endreymarcell/spoon"
+  url "https://github.com/endreymarcell/spoon/archive/v1.0.tar.gz"
+  sha256 "f44afdd294d838bd7fa0f4b8b28865122169f925cd6ffa7c68bfcd94d53e2015"
+
+  depends_on "peco"
+
+  def install
+    bin.install "spoon"
+  end
+end
