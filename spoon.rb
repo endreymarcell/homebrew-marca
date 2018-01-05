6d7d8a174b88b12045fc69aba890b7ec6977eb8f
Marcell Endrey <marcell.endrey@prezi.com>
2018-01-05 10:30:49 +0100

    [spoon] add csshx dependency

--- spoon.rb
+++ spoon.rb
@@ -5,6 +5,7 @@ class Spoon < Formula
   sha256 "f44afdd294d838bd7fa0f4b8b28865122169f925cd6ffa7c68bfcd94d53e2015"
 
   depends_on "peco"
+  depends_on "csshx"
 
   def install
     bin.install "spoon"
