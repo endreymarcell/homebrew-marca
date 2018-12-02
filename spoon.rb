6e1a22dd540a5e13bc229eb89094ebbb4e8f31fe
Marcell Endrey <marcell.endrey@prezi.com>
2018-12-02 18:42:41 +0100

    release spoon 2.0

--- spoon.rb
+++ spoon.rb
@@ -1,11 +1,10 @@
 class Spoon < Formula
   desc "SSH into EC2 nodes easily"
   homepage "https://github.com/endreymarcell/spoon"
-  url "https://github.com/endreymarcell/spoon/archive/v1.2.tar.gz"
-  sha256 "ce09c0f03d7efc896ae9a85400ae856abc9b3fe7ca9d789d79196dbc286f348e"
+  url "https://github.com/endreymarcell/spoon/archive/v2.0.tar.gz"
+  sha256 "56d1eda2c3eb9dcb91df61123cc212baa6a8fa71d6fe1c710a3df581f3fdf720"
 
   depends_on "awscli"
-  depends_on "csshx"
   depends_on "jq"
 
   def install
